746f8222456f1ddcc8242d94f913af79ae0b5604
mpavk <pavlenko.maksym.a@gmail.com>
2025-01-04 21:03:17 +0200

    Search Insert Position

--- Exercism/Rust/src/main.rs
+++ Exercism/Rust/src/main.rs
@@ -1,3 +1,16 @@
+struct Solution;
+
+impl Solution {
+    pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+        for i in 0..nums.len() {
+         if nums[i] == target || nums[i] > target {
+           return i as i32;
+         }
+        }
+        nums.len() as i32
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", Solution::search_insert(vec![1, 3, 4], 5));
 }
