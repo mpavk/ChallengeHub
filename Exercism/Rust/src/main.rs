6951753e72de58ac853267af5d3d78aab426e179
mpavk <pavlenko.maksym.a@gmail.com>
2025-01-10 00:04:59 +0200

    Armstrong_number

--- Exercism/Rust/src/main.rs
+++ Exercism/Rust/src/main.rs
@@ -1,9 +1,10 @@
-/// Check a Luhn checksum.
-///
+
 fn remove_whitespace(s: &str) -> String {
   s.chars().filter(|c| !c.is_whitespace()).collect()
 }
 
+/// Check a Luhn checksum.
+///
 pub fn is_valid_luhn_algorithm(code: &str) -> bool {
   let mut clear_code:String = remove_whitespace(code);
 
@@ -33,6 +34,15 @@ pub fn is_valid_luhn_algorithm(code: &str) -> bool {
   sum % 10 == 0
 }
 
+pub fn is_armstrong_number(num: u32) -> bool {
+  let mut string_num = num.to_string();
+  let mut sum = 0;
+  for i in string_num.chars() {
+    sum += i.to_digit(10).unwrap().pow(string_num.len() as u32);
+  }
+  sum == num
+}
+
 fn run_test_luhn_algorithm(){
   println!("{}", is_valid_luhn_algorithm("4539319503436467"));
   println!("{}", is_valid_luhn_algorithm("055 444 285"));
@@ -41,5 +51,5 @@ fn run_test_luhn_algorithm(){
 }
 
 fn main() {
-  run_test_luhn_algorithm();
+  // run_test_luhn_algorithm();
 }
