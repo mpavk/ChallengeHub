d0f9fc8ad3a22cb6168067854a7d672e5e24083b
mpavk <pavlenko.maksym.a@gmail.com>
2025-01-17 14:09:45 +0200

    start search solution task "is_valid_isbn"

--- Exercism/Rust/src/main.rs
+++ Exercism/Rust/src/main.rs
@@ -427,6 +427,33 @@ pub fn find_binary_search(array: &[i32], key: i32) -> Option<usize> {
   None
 }
 
+#[derive(Debug, PartialEq, Eq)]
+pub enum Error {
+  NotEnoughPinsLeft,
+  GameComplete,
+}
+
+pub struct BowlingGame {}
+
+impl BowlingGame {
+  pub fn new() -> Self {
+    todo!();
+  }
+
+  pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
+    todo!("Record that {pins} pins have been scored");
+  }
+
+  pub fn score(&self) -> Option<u16> {
+    todo!("Return the score if the game is complete, or None if not.");
+  }
+}
+
+pub fn is_valid_isbn(isbn: &str) -> bool {
+  todo!("Is {isbn:?} a valid ISBN number?");
+}
+
+
 
 
 fn main() {
