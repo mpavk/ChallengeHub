369c5fbf8fd017bcb2bebf1911e1bff57fd07556
mpavk <pavlenko.maksym.a@gmail.com>
2025-05-28 23:13:10 +0300

    Add solution "maximumValue" on cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
-int main(int, char**){
-    std::cout << "Hello, from challengehub!\n";
+using namespace std;
+
+class Solution {
+public:
+    int maximumValue(vector<string>& strs) {
+        int max = 0;
+        for (auto &str : strs ) {
+            for (int j = 0; j < str.length(); j++) {
+                if (( str[j]>= 65 && str[j] <= 90) || ( str[j]>= 97 && str[j] <= 122)) {
+                    if(str.length() > max){
+                        max = str.length();
+                    }
+                    break;
+                } else if (j == str.length()-1){
+                    if(stoi(str) > max){
+                        max = stoi(str);
+                        break;
+                    }
+                }
+            }
+        }
+        return max;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    Solution s;
+    return 0;
 }
