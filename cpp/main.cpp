28492fc99ff9c601d58eab0e43ffc22318044e25
mpavk <pavlenko.maksym.a@gmail.com>
2025-07-29 01:25:23 +0300

    git add solution 56 Merge Intervals

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -66,6 +66,7 @@ public:
             }
         }
         return max_sum;
+    }
 
     //217. Contains Duplicate
     bool containsDuplicate(vector<int>& nums) {
@@ -148,10 +149,44 @@ public:
         return result;
     }
 
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if (intervals.size() <= 1) {
+                return intervals;
+        }
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> result;
+        result.push_back(intervals[0]);
+
+        for(int i = 0; i < intervals.size(); i++) {
+            vector<int>& last_merged = result.back();
+            vector<int>& current = intervals[i];
+
+            if(last_merged[1] >= current[0]) {
+                last_merged[1] = max(last_merged[1], current[1]);
+            } else {
+                result.push_back(current);
+            }
+        }
+
+        return result;
+    }
 };
+
+void printVectorPairs(std::vector<std::vector<int>> &intervals){
+    for(auto &interval: intervals) {
+        std::cout<<interval[0] << " " << interval[1] << std::endl;
+    }
+}
 
 int main(int argc, char *argv[]) {
     Solution s;
     std::vector<int> nums = {1,0};
+    std::vector<std::vector<int>> intervals = {{1,3},{2,6},{8,10},{15,18}};
+    std::cout << intervals.size() << std::endl;
+
+    // printVectorPairs(intervals);
+    // sort(intervals.begin(), intervals.end());
+    // printVectorPairs(intervals);
+    s.merge(intervals);
     return 0;
 }
