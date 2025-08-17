8b05eec01fbcc6f3bfc7672a09381b4b6d054675
mpavk <pavlenko.maksym.a@gmail.com>
2025-08-17 22:49:50 +0300

    microfix

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -318,7 +318,6 @@ int main(int argc, char *argv[]) {
     std::vector<int> nums = {0,1,1,1};
     std::vector<std::vector<int>> intervals = {{1,3},{2,6},{8,10},{15,18}};
     std::cout << intervals.size() << std::endl;
-
     // printVectorPairs(intervals);
     // sort(intervals.begin(), intervals.end());
     // printVectorPairs(intervals);
