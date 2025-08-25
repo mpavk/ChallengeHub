758e97260bf68a7331597561eb9bf60fe3a33eee
mpavk <pavlenko.maksym.a@gmail.com>
2025-08-25 22:51:22 +0300

    Search Insert Position

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -351,9 +351,30 @@ public:
     }
 
     string largestWordCount(vector<string>& messages, vector<string>& senders) {
-
+        return "";
     }
 
+    int searchInsert(std::vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+
+            else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
 };
 
 int main(int argc, char *argv[]) {
