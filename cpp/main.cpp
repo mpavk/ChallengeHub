f8a6131ad26bee7eda89af9a4410f438d5d8e690
mpavk <pavlenko.maksym.a@gmail.com>
2025-09-22 00:51:25 +0300

    add solution

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -391,15 +391,19 @@ public:
     }
 
     bool repeatedSubstringPattern(string s) {
-
+        if (s.empty()) {
+                return false;
+        }
+        std::string t = s + s;
+        return t.find(s, 1) != s.length();
     }
 };
 
 int main(int argc, char *argv[]) {
     Solution s;
+    string str = "Hello World";
     std::vector<int> nums = {3,3};
     std::vector<std::vector<int>> intervals = {{1,3},{2,6},{8,10},{15,18}};
-    printVector(nums);
-    std::cout<< s.lengthOfLastWord("Hello World"s);
+    std::cout <<s.repeatedSubstringPattern("abac"s) <<std::endl;
     return 0;
 }
